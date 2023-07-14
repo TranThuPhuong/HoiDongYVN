2e2d0768c26a551c6b6a202c0532e9d5f333cc11
TranThuPhuong <thuphuongtran767@gmail.com>
2023-07-14 20:45:41 +0700

    thêm giao diện admin

--- HoiDongYVN/Startup.cs
+++ HoiDongYVN/Startup.cs
@@ -49,6 +49,13 @@ namespace HoiDongYVN
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
